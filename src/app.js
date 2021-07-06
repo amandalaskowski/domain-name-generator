ef595fa86b02d56e7629ee1c83579870a8bbe803
amandalaskowski <amanda.2002@aol.com>
2021-07-06 18:46:19 +0000

    my code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let first = ["my", "your", "their"];
+  let second = ["crazy", "funny", "stupid"];
+  let third = ["sister", "friend", "dog"];
+  let fourth = [".com", ".org", ".net"];
+  for (let f = 0; f < first.length; f++) {
+    for (let s = 0; s < second.length; s++) {
+      for (let t = 0; t < third.length; t++) {
+        for (let i = 0; i < fourth.length; i++) {
+          console.log(first[f] + second[s] + third[t] + fourth[i]);
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
