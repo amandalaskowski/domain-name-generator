f47c3fb6c1358c9ec7328ac708c02d745be7de20
amandalaskowski <amanda.2002@aol.com>
2021-07-07 21:37:03 +0000

    update

--- src/app.js
+++ src/app.js
@@ -6,23 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  function generateExcuse() {
+  function generateDomain() {
     let first = ["my", "your", "their"];
     let second = ["crazy", "funny", "stupid"];
     let third = ["sister", "friend", "dog"];
     let fourth = [".com", ".org", ".net"];
 
+    let myArray = [];
+
     for (let f = 0; f < first.length; f++) {
       for (let s = 0; s < second.length; s++) {
         for (let t = 0; t < third.length; t++) {
           for (let i = 0; i < fourth.length; i++) {
-            let total = first[f] + second[s] + third[t] + fourth[i];
-            return total;
+            myArray.push(first[f] + second[s] + third[t] + fourth[i]);
+            return myArray;
           }
         }
       }
     }
   }
 
-  document.getElementById("names").innerHTML = generateExcuse();
+  document.getElementById("names").innerHTML = generateDomain();
 };
