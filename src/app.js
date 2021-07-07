9b722de6a3a37bf6b9627b8be951b840f6bae697
amandalaskowski <amanda.2002@aol.com>
2021-07-07 01:07:41 +0000

    still working on it

--- src/app.js
+++ src/app.js
@@ -6,18 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let first = ["my", "your", "their"];
-  let second = ["crazy", "funny", "stupid"];
-  let third = ["sister", "friend", "dog"];
-  let fourth = [".com", ".org", ".net"];
-  for (let f = 0; f < first.length; f++) {
-    for (let s = 0; s < second.length; s++) {
-      for (let t = 0; t < third.length; t++) {
-        for (let i = 0; i < fourth.length; i++) {
-          console.log(first[f] + second[s] + third[t] + fourth[i]);
+  function generateExcuse() {
+    let first = ["my", "your", "their"];
+    let second = ["crazy", "funny", "stupid"];
+    let third = ["sister", "friend", "dog"];
+    let fourth = [".com", ".org", ".net"];
+
+    for (let f = 0; f < first.length; f++) {
+      for (let s = 0; s < second.length; s++) {
+        for (let t = 0; t < third.length; t++) {
+          for (let i = 0; i < fourth.length; i++) {
+            let total = first[f] + second[s] + third[t] + fourth[i];
+            return total;
+          }
         }
       }
     }
   }
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("names").innerHTML = generateExcuse();
 };
